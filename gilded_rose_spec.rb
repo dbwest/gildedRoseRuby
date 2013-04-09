92bb292c8dff74a7ede90179a1fc51ad07da2b82
David West <david.b.west@gmail.com>
2013-04-09 17:17:21 -0400

    more tests in place

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -35,9 +35,36 @@ describe GildedRose do
     subject.items[2].quality.should be > -1
   end
 
+  it "should not let sulfuras degrade in quality" do
+      old_quality = subject.items[3].quality
+      2.times {subject.update_quality}
+      new_quality = subject.items[3].quality
+      new_quality.should equal old_quality
+  end
 
+  it "should not let any regular item have a value more than 50" do
+      52.times {subject.update_quality}
+      subject.items[1].quality.should be <= 50
+  end
 
+  it "should make the sellby date for Dexterity Vest change" do
+      sellby_old = subject.items[0].sell_in
+      subject.update_quality
+      sellby_new = subject.items[0].sell_in
+      sellby_new.should equal (sellby_old - 1)
+  end
 
+  it "should make the sellby date for the elixir change" do
+      sellby_old = subject.items[2].sell_in
+      subject.update_quality
+      sellby_new = subject.items[2].sell_in
+      sellby_new.should equal (sellby_old - 1)
+  end
 
-
+  it "should make the sellby date for the backstage passes change" do
+      sellby_old = subject.items[4].sell_in
+      subject.update_quality
+      sellby_new = subject.items[4].sell_in
+      sellby_new.should equal (sellby_old - 1)
+  end
 end
